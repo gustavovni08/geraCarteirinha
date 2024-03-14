4ff088c08474075e3435586c00990bd41af3147d
gustavovni08 <gustavovni08@gmail.com>
2024-03-14 15:47:13 -0300

    server

--- src/views/GeraCarteirinaEmLote.js
+++ src/views/GeraCarteirinaEmLote.js
@@ -1,9 +1,18 @@
 import UploadCard from "../components/upload/UploadCard";
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height:100vh;
+`
+
 function GeraCarteirinhaEmLote(){
     return(
-        <div>
+        <MainContainer>
             <UploadCard/>
-        </div>
+        </MainContainer>
         
     )
 }
